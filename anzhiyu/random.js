1973b99bbab7de7bf895322d3a4a8e0be8faa981
Epiphany0130 <sdguyq@163.com>
2025-06-26 08:45:40 +0800

    Site updated: 2025-06-26 08:45:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/26/hello-world/"];function toRandomPost(){
+var posts=["2025/06/26/hello-world/","2025/06/26/My-New-Post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
